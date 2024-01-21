5c2dccfa8fd626bfb91e835b1e88a7d2889fa2dd
elgenio <rickentchabet@gmail.com>
2024-01-21 09:56:45 +0100

    First commit

--- main.py
+++ main.py
@@ -1,51 +1,50 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error
-from dataset import treat_dataset
+from dataset_house import treat_dataset
 from model import create_predict_price_model
 #import tensorflow as tf
 def main():
+
+    print("Predictiong prices")
         
-    df = pd.read_csv('house-prices-advanced-regression-techniques/train.csv')
-    df = treat_dataset(df)
-    for column, dtype in df.dtypes.items():
-        if dtype != 'object':
-            print(f"{column}: {dtype}")
+    train_data = pd.read_csv('house-prices-advanced-regression-techniques/train.csv')
+    train_data = treat_dataset(train_data)
 
     selected_features = ['MSSubClass', 'LotFrontage','LotArea','OverallQual','OverallCond','YearBuilt',  
                         'YearRemodAdd','MasVnrArea','BsmtFinSF1','BsmtFinSF2','BsmtUnfSF','TotalBsmtSF',
                         '1stFlrSF','2ndFlrSF','LowQualFinSF','GrLivArea','BsmtFullBath','BsmtHalfBath',  
                         'FullBath','HalfBath','KitchenAbvGr','KitchenAbvGr','TotRmsAbvGrd','Fireplaces', 
                         'GarageYrBlt','GarageCars','GarageArea','WoodDeckSF','OpenPorchSF','EnclosedPorch',  
-                        '3SsnPorch','ScreenPorch','PoolArea','MiscVal','MoSold','YrSold','SalePrice',  
+                        '3SsnPorch','ScreenPorch','PoolArea','MiscVal','MoSold','YrSold',  
                         'PoolQC_encoded','Fence_encoded','PavedDrive_encoded',
                         ]
     target_variable = 'SalePrice'
 
-    X = df[selected_features]
-    y = df[target_variable]
+    X = train_data[selected_features]
+    y = train_data[target_variable]
 
     # Split the data into training and testing sets
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-    model, scaler,X_test_scaled = create_predict_price_model(X_train,X_test,y_train)
-    # Make predictions on the test set
-    predictions = model.predict(X_test_scaled)
+    model= create_predict_price_model(X_train,y_train)
 
     # Evaluate the model
+    predictions = model.predict(X_test)
     mse = mean_squared_error(y_test, predictions)
-    rmse = mse**0.5
-    print(f"Root Mean Squared Error (RMSE): {rmse}")
+    print(f'Mean Squared Error on Validation Set: {mse}')
 
-    new_data = pd.DataFrame([[8,1,2,3,4,5,6,7,8,9,10,1,1,1,1,1,1,1,1,1,1,1,1, 2000, 2, 1000, 1200, 4,2, 6, 1990, 2005, 1, 800, 1, 200, 500,2010, 1, 8
-                            ]], columns=selected_features)
+    test_data = pd.read_csv('house-prices-advanced-regression-techniques/test.csv')
+    test_data = treat_dataset(test_data)
 
-    new_data_scaled = scaler.transform(new_data)
-    predicted_price = model.predict(new_data_scaled)
-    print(f"Predicted Sale Price for new data: {predicted_price[0]}")
+    test_predictions = model.predict(test_data[selected_features])
+    result_set = pd.DataFrame({'Id': test_data['Id'], 'SalePrice': test_predictions})
+
+    result_set.to_csv('output/predictions.csv', index=False)
+
+    print("Prices predicted successfully and saved in output/predictions.csv")
 
 if __name__ == "__main__":
     main()
 
-    #print(df)
 
--- model.py
+++ model.py
@@ -1,15 +1,8 @@
 from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import LinearRegression
 
-def create_predict_price_model(X_train, X_test,y_train):
-    # Standardize the features (optional but often recommended)
-    scaler = StandardScaler()
-    X_train_scaled = scaler.fit_transform(X_train)
-    X_test_scaled = scaler.transform(X_test)
-
-    # Create a linear regression model
+def create_predict_price_model(X_train,y_train):
     model = LinearRegression()
-
-    # Train the model
-    model.fit(X_train_scaled, y_train)
-    return model, scaler,X_test_scaled
+    model.fit(X_train, y_train)
+    
+    return model
